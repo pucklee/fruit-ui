7f2c53e336422a3338f1772edf498b63848ddd9b
pucklee <497003812@qq.com>
2016-07-28 18:17:04 +0800

    header修改

--- example/component/header/index.js
+++ example/component/header/index.js
@@ -6,27 +6,47 @@ import './styles.less';
 export default class Header extends React.Component {
   static propTypes = {
     title: React.PropTypes.string,
+    backBtn: React.PropTypes.string,
+    menuBtn: React.PropTypes.string,
+    menuClick: React.PropTypes.func,
+    backwards: React.PropTypes.func
   }
 
   static defaultProps = {
     title: document.title,
+    menuClick: null,
+    backwards: null
+  }
+
+  backAction = () => {
+    if(backwards){
+      backwards()
+    }else{
+      history.go(-1)
+    }
+  }
+
+  menuAction = () => {
+    if(menuClick){
+      menuClick()
+    }
   }
 
   render() {
     let className = "f-header";
 
-    const {title} = this.props;
+    const {title,backBtn,menuBtn} = this.props;
 
     return (
       <header className={className}>
-        <div className="f-header-left">
-
+        <div className="f-header-left" onClick={this.backAction}>
+          {backBtn}
         </div>
         <h1 className="f-header-title">
           {title}
         </h1>
-        <div className="f-header-right">
-
+        <div className="f-header-right" onClick={this.menuAction}>
+          {menuBtn}
         </div>
       </header>
     );
